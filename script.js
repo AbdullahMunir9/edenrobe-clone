20a4ce30cf740da0141164e2ffb514e93bfa90fa
Abdullah Munir <abdullahmunir467@gmail.com>
2024-09-18 19:47:45 +0500

    Drop Down added

--- script.js
+++ script.js
@@ -76,3 +76,50 @@ function whileScroll(){
 }
 
 window.addEventListener("scroll",whileScroll);
+
+var dropdown = document.getElementsByClassName("showDropDown");
+
+// First Drop Down
+
+dropdown[0].addEventListener("mouseenter",()=>{
+    document.getElementsByClassName("NavdropDown")[0].style.display = "block";
+})
+dropdown[0].addEventListener("mouseleave",()=>{
+    document.getElementsByClassName("NavdropDown")[0].style.display = "none";
+})
+
+// Second Drop Down
+
+dropdown[1].addEventListener("mouseenter",()=>{
+    document.getElementsByClassName("NavdropDown")[1].style.display = "block";
+})
+dropdown[1].addEventListener("mouseleave",()=>{
+    document.getElementsByClassName("NavdropDown")[1].style.display = "none";
+})
+
+// Third Drop Down
+
+dropdown[2].addEventListener("mouseenter",()=>{
+    document.getElementsByClassName("NavdropDown")[2].style.display = "block";
+})
+dropdown[2].addEventListener("mouseleave",()=>{
+    document.getElementsByClassName("NavdropDown")[2].style.display = "none";
+})
+
+// Fourth Drop Down
+
+dropdown[3].addEventListener("mouseenter",()=>{
+    document.getElementsByClassName("NavdropDown")[3].style.display = "block";
+})
+dropdown[3].addEventListener("mouseleave",()=>{
+    document.getElementsByClassName("NavdropDown")[3].style.display = "none";
+})
+
+// Fifth Drop Down
+
+dropdown[4].addEventListener("mouseenter",()=>{
+    document.getElementsByClassName("NavdropDown")[4].style.display = "block";
+})
+dropdown[4].addEventListener("mouseleave",()=>{
+    document.getElementsByClassName("NavdropDown")[4].style.display = "none";
+})
