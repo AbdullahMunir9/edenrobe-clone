859d63566de6f95f3e344927796bb994b021cd5f
Abdullah Munir <abdullahmunir467@gmail.com>
2024-10-09 22:29:01 +0500

    responsiveness added

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var Navbar = document.getElementById("NavBar");
-var Navlogo = document.getElementById("nav_logo");
+var Navlogo = document.getElementsByClassName("nav_logo");
 
 var navUL = document.getElementById("Nav_ul");
 var navListitems = navUL.getElementsByTagName("li");
@@ -11,8 +11,10 @@ var items_nav_cont3 = document.getElementsByTagName("i")
 
 Navbar.addEventListener("mouseenter",()=>{
     Navbar.classList.add("scrolled");
-    Navlogo.src = 'Images/black_logo.png';
-
+    for(let i=0 ;i<Navlogo.length; i++){
+        Navlogo[i].src = 'Images/black_logo.png';
+    }
+    
     for(let i = 0; i < navListitems.length; i++) {
         var nav_anchor = navListitems[i].getElementsByTagName("a");
         nav_anchor[0].style.color = 'black';
@@ -31,7 +33,10 @@ Navbar.addEventListener("mouseenter",()=>{
 Navbar.addEventListener("mouseleave",()=>{
     if(window.scrollY<50){
         Navbar.classList.remove("scrolled");
-        Navlogo.src = 'Images/white-logo.png';
+
+        for(let i=0 ;i<Navlogo.length; i++){
+            Navlogo[i].src = 'Images/white-logo.png';
+        }
             
         for(let i = 0; i < navListitems.length; i++) {
             var nav_anchor = navListitems[i].getElementsByTagName("a");
@@ -53,7 +58,9 @@ Navbar.addEventListener("mouseleave",()=>{
 function whileScroll(){
     if(window.scrollY>50){
         Navbar.classList.add("scrolled");
-        Navlogo.src = 'Images/black_logo.png';
+        for(let i=0 ;i<Navlogo.length; i++){
+            Navlogo[i].src = 'Images/black_logo.png';
+        }
 
         for(let i = 0; i < navListitems.length; i++) {
             var nav_anchor = navListitems[i].getElementsByTagName("a");
@@ -71,7 +78,10 @@ function whileScroll(){
     }
     else{
         Navbar.classList.remove("scrolled");
-        Navlogo.src = 'Images/white-logo.png';
+        
+        for(let i=0 ;i<Navlogo.length; i++){
+            Navlogo[i].src = 'Images/white-logo.png';
+        }
         
         for(let i = 0; i < navListitems.length; i++) {
             var nav_anchor = navListitems[i].getElementsByTagName("a");
