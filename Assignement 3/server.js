597764ce623c2f91fcd54b818f636a335c21e9d3
Abdullah Munir <abdullahmunir467@gmail.com>
2024-12-24 10:15:35 +0500

    assignments uploaded

--- Assignement 3/server.js
+++ Assignement 3/server.js
@@ -1,53 +1,42 @@
-const express = require("express");
-let app  = express();
+const express  = require("express");
+let app = express();
 
-app.set("view engine","ejs");
+// add a middleware to parse body data for form submission
+app.use(express.urlencoded());
+
+var expressLayouts = require("express-ejs-layouts");
+app.use(expressLayouts);
+
 app.use(express.static("public"));
+app.set("view engine",'ejs');
+
+let productsRouter = require("./routes/admin/products.router");
+app.use(productsRouter);
 
 
-app.get("/CV",(req,res)=>{
-    res.render("CV");
+
+
+const mongoose = require("mongoose");
+let connectionString = "mongodb://127.0.0.1:27017/Edenrobe"; //since i have ipv4 version of mongodb
+// let connectionString = "mongodb://localhost:27017/Edenrobe"; // use this when u have ipv6 version
+mongoose.connect(connectionString)
+.then(()=>{
+    console.log(`Connected To:${connectionString}`)
+})
+.catch((err)=>{
+    console.log("hello");
 })
 
 app.get("/",(req,res)=>{
-    res.render("home");
+    res.render("pages/Main_Site_pages/landingPage");
 })
 
-
-
-
-
-app.get("/about",(req,res)=>{
-    res.send("about")
+app.get("/women",async (req,res)=>{
+    let Products = require("./models/products.models");
+    let product = await Products.find();
+    res.render("pages/Main_Site_pages/women",{ product });
 })
 
-app.listen(5000,() => {
-    console.log("Server started at location: 5000")
-})
-
-
-
-
-// const express = require("express");
-// let app  = express();
-
-
-
-// app.get("/contact-us",(req,res)=>{
-//     res.send("<h1> Hello Section B Contact</h1>")
-// })
-
-// app.get("/",(req,res)=>{
-//     res.send("<h1> Hello Section A Home</h1>")
-// })
-
-// app.get("/update",(req,res)=>{
-//     res.send("<h1> Hello Section update</h1>")
-// })
-// app.get("/create",(req,res)=>{
-//     res.send("<h1> Hello create</h1>")
-// })
-
-// app.listen(5001,() => {
-//     console.log("Server started at location: 5001")
-// })
+app.listen(5000, ()=>{
+    console.log("Server started at location : 5000");
+});
